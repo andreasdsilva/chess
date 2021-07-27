0d5ae22e37e66f6dca2f9ac67bfe2d32fc07641e
masort1 <andreas.silva@universo.univates.br>
2021-07-27 10:57:19 -0300

    Board game

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,11 @@
+import Chess.ChessMatch;
 
 public class Application {
 
 	public static void main(String[] args) {
-		
+			
+		ChessMatch chessMatch = new ChessMatch();	
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
