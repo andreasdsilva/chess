422771f9a87fa2c6f153d7a36c5ed1d3e1fd057f
masort1 <andreas.silva@universo.univates.br>
2021-08-18 09:14:03 -0300

    Clear Screen

--- src/Application/program.java
+++ src/Application/program.java
@@ -1,13 +1,41 @@
 package Application;
-import Chess.ChessMatch;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class program{
+import Chess.ChessException;
+import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+
+public class Program{
 
 	public static void main(String[] args) {
-			
+		
+		Scanner input = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();	
-		UI.printBoard(chessMatch.getPieces());
-
+		
+		while (true) {
+			try {
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(input);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(input);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch(ChessException e) {
+				System.out.println(e.getMessage());
+				input.nextLine();
+			}
+			catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				input.nextLine();
+			}
+		}
 	}
-
 }
